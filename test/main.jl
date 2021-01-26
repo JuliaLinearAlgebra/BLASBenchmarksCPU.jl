48ed3d398b03d3558673f975dfee1f3e905d9872
Dilum Aluthge <dilum@aluthge.com>
2021-01-26 15:31:43 -0500

    Add a test for `get_benchmark_type`

--- test/main.jl
+++ test/main.jl
@@ -12,6 +12,7 @@ for T in [Float64, Float32]
         threaded = threaded,
     )
     @test benchmark_result isa BLASBenchmarksCPU.BenchmarkResult
+    @test get_benchmark_type(benchmark_result) === T
     plot_directory = mktempdir()
     BLASBenchmarksCPU.plot(
         benchmark_result;
