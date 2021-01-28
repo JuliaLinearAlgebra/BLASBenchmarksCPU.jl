2faaeee0fd6684fbdea487735320c7ceefd42032
Chris Elrod <elrodc@gmail.com>
2021-01-28 12:05:36 -0500

    Remove invalid argument

--- test/interface.jl
+++ test/interface.jl
@@ -2,7 +2,7 @@
 import BLASBenchmarksCPU
 import StatsPlots
 @testset "Interface" begin
-    benchmark_result = BLASBenchmarksCPU.runbench(Float64; sizes = [1, 2, 5, 10, 20, 50, 100, 200], threaded=false, summarystat = BLASBenchmarksCPU.median) #test that threads=false at least doesn't throw somewhere.
+    benchmark_result = BLASBenchmarksCPU.runbench(Float64; sizes = [1, 2, 5, 10, 20, 50, 100, 200], threaded=false) #test that threads=false at least doesn't throw somewhere.
     dfmin = BLASBenchmarksCPU.benchmark_result_df(benchmark_result) # minimum
     dfmedian = BLASBenchmarksCPU.benchmark_result_df(benchmark_result, :median)
     dfmean = BLASBenchmarksCPU.benchmark_result_df(benchmark_result, :mean)
